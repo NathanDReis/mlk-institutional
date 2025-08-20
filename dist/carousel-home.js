f6f6a76cc7b0b06507657fa553fe525892f51716
NathanDReis <nathandavidreis@gmail.com>
2025-08-19 22:56:51 -0300

    feat(home): alterando botão de saiba mais e título

--- dist/carousel-home.js
+++ dist/carousel-home.js
@@ -1,31 +1,35 @@
 "use strict";
 let currentPosition = 0;
+const linkGeralLearnMore = document.querySelector('#link_geral_learn_more');
 const items = [
     {
         position: 0,
         el: document.getElementById('carousel-item-1'),
-        title: document.getElementById('carousel-title-1'),
+        titles: document.querySelectorAll('.carousel-title-1'),
+        link: '#system-cftv',
     },
     {
         position: 1,
         el: document.getElementById('carousel-item-2'),
-        title: document.getElementById('carousel-title-2'),
+        titles: document.querySelectorAll('.carousel-title-2'),
+        link: '#technology',
     },
     {
         position: 2,
         el: document.getElementById('carousel-item-3'),
-        title: document.getElementById('carousel-title-3'),
+        titles: document.querySelectorAll('.carousel-title-3'),
+        link: '#suport',
     },
     {
         position: 3,
         el: document.getElementById('carousel-item-4'),
-        title: document.getElementById('carousel-title-4'),
+        titles: document.querySelectorAll('.carousel-title-4'),
+        link: '#personalization',
     },
 ];
+linkGeralLearnMore.href = items[currentPosition].link;
 const $prevButton = document.getElementById('data-carousel-prev');
 const $nextButton = document.getElementById('data-carousel-next');
-const $prevButtonMobile = document.getElementById('data-carousel-prev-mobile');
-const $nextButtonMobile = document.getElementById('data-carousel-next-mobile');
 $nextButton.onclick = () => {
     currentPosition += 1;
     if (currentPosition >= items.length) {
@@ -40,28 +44,19 @@ $prevButton.onclick = () => {
     }
     updateCarousel();
 };
-$nextButtonMobile.onclick = () => {
-    currentPosition += 1;
-    if (currentPosition >= items.length) {
-        currentPosition = 0;
-    }
-    updateCarousel();
-};
-$prevButtonMobile.onclick = () => {
-    currentPosition -= 1;
-    if (currentPosition <= -1) {
-        currentPosition = items.length - 1;
-    }
-    updateCarousel();
-};
 function updateCarousel() {
     items.forEach((item) => {
         item.el.classList.remove('flex');
         item.el.classList.add('hidden');
-        item.title.classList.add('hidden');
+        item.titles.forEach((title) => {
+            title.classList.add('hidden');
+        });
     });
     items[currentPosition].el.classList.add('flex');
     items[currentPosition].el.classList.remove('hidden');
-    items[currentPosition].title.classList.remove('hidden');
+    items[currentPosition].titles.forEach((title) => {
+        title.classList.remove('hidden');
+    });
+    linkGeralLearnMore.href = items[currentPosition].link;
 }
 //# sourceMappingURL=carousel-home.js.map
